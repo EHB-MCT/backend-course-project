71958526c49ad7ab70c1b57a9a5639ccb898f8ea
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-07-06 22:49:53 +0200

    User factory and seeder

--- FeedbackTool/database/seeders/DatabaseSeeder.php
+++ FeedbackTool/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $number = random_int(10, 20);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory($number)->create();
+
+        $user = new User();
+        $user->name = 'Some User';
+        $user->email = 'user@gmail.com';
+        $user->password = Hash::make('password');
+        $user->save();
     }
 }
