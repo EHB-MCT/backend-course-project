53b69116f9a0245ef08ab0c220f8f9a1eb0a63f6
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-07-06 22:52:08 +0200

    Survey factory and seeder

--- FeedbackTool/database/factories/SurveyFactory.php
+++ FeedbackTool/database/factories/SurveyFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class SurveyFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => random_int(1,User::all()->count()),
+            'list_name' => $this->faker->safeColorName(),
         ];
     }
 }
--- FeedbackTool/database/seeders/DatabaseSeeder.php
+++ FeedbackTool/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Survey;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,11 +20,17 @@ class DatabaseSeeder extends Seeder
         $number = random_int(10, 20);
 
         User::factory($number)->create();
+        Survey::factory(random_int(User::all()->count()*2,User::all()->count()*3))->create();
 
         $user = new User();
         $user->name = 'Some User';
         $user->email = 'user@gmail.com';
         $user->password = Hash::make('password');
         $user->save();
+
+        $survey = new Survey();
+        $survey->user_id = 1;
+        $survey->list_name = 'Lijstje 1';
+        $survey->save();
     }
 }
