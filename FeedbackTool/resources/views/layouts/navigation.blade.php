3c9e3ab261db3f1bd230782f39c176eea9a85f64
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-07-07 17:31:50 +0200

    Added a survey navigation button

--- FeedbackTool/resources/views/layouts/navigation.blade.php
+++ FeedbackTool/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('surveys')" :active="request()->routeIs('surveys', 'survey')">
+                        {{ __('Surveys') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- FeedbackTool/resources/views/surveys.blade.php
+++ FeedbackTool/resources/views/surveys.blade.php
@@ -11,7 +11,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <a href="{{ route('survey', ['id' => $survey->id]) }}">{{$survey->id}}</a> {{$survey->survey_name}}
+                        <a href="{{ route('survey', ['id' => $survey->id]) }}">{{$survey->id}} {{$survey->survey_name}}</a>
                     </div>
                 </div>
             </div>
