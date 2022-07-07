5ac59f7e26101e90af1aabbc09eb5c474348b288
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-07-07 14:08:03 +0200

    Show questions
    Show the questions on the detail page

--- FeedbackTool/app/Http/Controllers/SurveyController.php
+++ FeedbackTool/app/Http/Controllers/SurveyController.php
@@ -10,13 +10,19 @@ class SurveyController extends Controller
 {
     public static function all()
     {
+        // Get all surveys
         return Survey::all();
     }
 
     public static function detail($id)
     {
+        // Get the survey with this id
         $survey = Survey::firstWhere('id', $id);
-        
+
+        // Get the questions corresponding to this survey
+        $survey->questions = Question::where('survey_id', $survey->id)->get();
+
+        // return the survey
         return $survey ;
     }
 }
