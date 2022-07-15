f0563e4b58c96e2fd2f4799da465ee959d12368e
ehbsacha <71716345+ehbsacha@users.noreply.github.com>
2022-07-15 16:21:46 +0200

    guest layouts
    When not logged in you are on the  guest page.
    I made the login, register, update password page layout

--- FeedbackTool/resources/views/layouts/guest.blade.php
+++ FeedbackTool/resources/views/layouts/guest.blade.php
@@ -8,13 +8,17 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/11db2a43ab.js" crossorigin="anonymous"></script>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body class="guest">
+        <div>
+            <a class="center" href="/">
+                <x-application-logo />
+            </a>
             {{ $slot }}
         </div>
     </body>
