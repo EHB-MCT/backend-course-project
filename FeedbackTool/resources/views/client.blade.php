0d4f25416798016f165136a38392f62d69f2cf3a
ehbsacha <sacha.voskuilen@student.ehb.be>
2022-08-14 23:07:52 +0200

    Added colours to the graphs

--- FeedbackTool/resources/views/client.blade.php
+++ FeedbackTool/resources/views/client.blade.php
@@ -23,17 +23,51 @@
         const ctx = document.getElementById('myChart').getContext('2d');
 
         var survey = {!! $user->survlists[0] !!}
+        console.log(survey);
+
+        var colours = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+        ]
+
+        var borderColours = [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+        ]
+
 
         function fillDatasets(){
-            var allDatasets = []
+
+            var allDatasets = [];
 
             for (let index = 0; index < survey.scores.length; index++) {
+
+                var itemBackgroundColor = '';
+                var itemBorderColor = '';
+                var i = 0;
+
+                do {
+                    if(colours[index - (colours.length * i)]){
+                        itemBackgroundColor = colours[index - (colours.length * i)];
+                        itemBorderColor = borderColours[index - (colours.length * i)];
+                    } else {
+                        i++;
+                    }
+                } while (!itemBackgroundColor);
 
                 var set = {
                     label : survey.questions[index],
                     data : survey.scores[index],
-                    backgroundColor : 'rgba(255, 99, 132, 0.2)',
-                    borderColor : 'rgba(255, 99, 132, 1)',
+                    backgroundColor : itemBackgroundColor,
+                    borderColor : itemBorderColor,
                     borderWidth : 1
                 }
 
